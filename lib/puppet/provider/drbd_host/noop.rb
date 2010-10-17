daac13d108964007d868769b4bf4511c9e8e4ca2
Tim Sharpe <tim@github.com>
2010-10-17 14:01:54 +1100

    Avoid loading Augeas at all here

--- lib/puppet/provider/drbd_host/noop.rb
+++ lib/puppet/provider/drbd_host/noop.rb
@@ -1,27 +1,8 @@
-require 'augeas' rescue nil
-
-class Augeas
-  unless Augeas.public_methods.include? "three_arg_open"
-    class << self
-      alias_method :three_arg_open, :open
-    end
-
-    def self.open(root, search_path="", flags=0)
-      three_arg_open(root, search_path, flags)
-    end
-  end
-end
-
 Puppet::Type.type(:drbd_host).provide(:noop) do
   desc "Dummy provider to be used on the Puppet runs before the DRBD " +
        "Augeas lens is installed."
 
-  confine :false, @aug.exists("/augeas/load/Drbd/lens")
-
-  def initialize(*args)
-    super
-    @aug = Augeas.open("/")
-  end
+  confine :false => File.exists? "/usr/share/augeas/lenses/drbd.aug"
 
   def create
   end
