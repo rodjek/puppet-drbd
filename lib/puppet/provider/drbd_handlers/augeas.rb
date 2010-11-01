4fafef095707fdc8eac333db0ee56e5d84560f8f
Tim Sharpe <tim@github.com>
2010-11-01 17:31:56 +1100

    DRY drbd_handlers

--- lib/puppet/provider/drbd_handlers/augeas.rb
+++ lib/puppet/provider/drbd_handlers/augeas.rb
@@ -29,99 +29,79 @@ Puppet::Type(:drbd_handlers).provide(:noop) do
     @aug.save
   end
 
+  def get_val(key)
+    @aug.get("#{@context}/handlers/#{key}")
+  end
+
+  def set_val(key, value)
+    if value.nil?
+      @aug.rm("#{@context}/handlers/#{key}")
+    else
+      @aug.set("#{@context}/handlers/#{key}", value.to_s)
+    end
+  end
+
   def pri_on_incon_degr
-    @aug.get("#{@context}/handlers/pri-on-incon-degr")
+    get_val('pri-on-incon-degr')
   end
 
   def pri_on_incon_degr=(value)
-    if value.nil?
-      @aug.rm("#{@context}/handlers/pri-on-incon-degr")
-    else
-      @aug.set("#{@contetx}/handlers/pri-on-incon-degr")
-    end
+    set_val('pri-on-incon-degr', value)
   end
 
   def pri_lost_after_sb
-    @aug.get("#{@context}/handlers/pri-lost-after-sb")
+    get_val('pri-lost-after-sb')
   end
 
   def pri_lost_after_sb=(value)
-    if value.nil?
-      @aug.rm("#{@context}/handlers/pri-lost-after-sb")
-    else
-      @aug.set("#{@context}/handlers/pri-lost-after-sb", value)
-    end
+    set_val('pri-lost-after-sb', value)
   end
 
   def pri_lost
-    @aug.get("#{@context}/handlers/pri-lost")
+    get_val('pri-lost')
   end
 
   def pri_lost=(value)
-    if value.nil?
-      @aug.rm("#{@context}/handlers/pri-lost")
-    else
-      @aug.set("#{@context}/handlers/pri-lost", value)
-    end
+    set_val('pri-lost', value)
   end
 
   def outdate_peer
-    @aug.get("#{@context}/handlers/outdate-peer")
+    get_val('outdate-peer')
   end
 
   def outdate_peer=(value)
-    if value.nil?
-      @aug.rm("#{@context}/handlers/outdate-peer")
-    else
-      @aug.set("#{@context}/handlers/outdate-peer", value)
-    end
+    set_val('outdate-peer', value)
   end
 
   def local_io_error
-    @aug.get("#{@context}/handlers/local-io-error")
+    get_val('local-io-error')
   end
 
   def local_io_error=(value)
-    if value.nil?
-      @aug.rm("#{@context}/handlers/local-io-error")
-    else
-      @aug.set("#{@context}/handlers/local-io-error", value)
-    end
+    set_val('local-io-error', value)
   end
 
   def split_brain
-    @aug.get("#{@context}/handlers/split-brain")
+    get_val('split-brain')
   end
 
   def split_brain=(value)
-    if value.nil?
-      @aug.rm("#{@context}/handlers/split-brain")
-    else
-      @aug.set("#{@context}/handlers/split-brain", value)
-    end
+    set_val('split-brain', value)
   end
 
   def before_resync_target
-    @aug.get("#{@context}/handlers/before-resync-target")
+    get_val('before-resync-target')
   end
 
   def before_resync_target=(value)
-    if value.nil?
-      @aug.rm("#{@context}/handlers/before-resync-target")
-    else
-      @aug.set("#{@context}/handlers/before-resync-target", value)
-    end
+    set_val('before-resync-target', value)
   end
 
   def after_resync_target
-    @aug.get("#{@context}/handlers/after-resync-target")
+    get_val('after-resync-target')
   end
 
   def after_resync_target=(value)
-    if value.nil?
-      @aug.rm("#{@context}/handlers/after-resync-target")
-    else
-      @aug.set("#{@context}/handlers/after-resync-target", value)
-    end
+    set_val('after-resync-target', value)
   end
 end
